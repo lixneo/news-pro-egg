c5ec3f74abb9bb138c8f3e42058facc411dc7e15
lixneo <15806155815@163.com>
2025-01-03 16:04:55 +0800

    update

--- config/config.default.ts
+++ config/config.default.ts
@@ -1,11 +1,11 @@
-import { EggAppConfig, EggAppInfo, PowerPartial } from 'egg';
+import { EggAppConfig, EggAppInfo, PowerPartial } from "egg";
 
 export default (appInfo: EggAppInfo) => {
   const config = {} as PowerPartial<EggAppConfig>;
 
   // override config from framework / plugin
   // use for cookie sign key, should change to your own and keep security
-  config.keys = appInfo.name + '_1735269614302_1771';
+  config.keys = appInfo.name + "_1735269614302_1771";
 
   // add your egg config in here
   config.middleware = [];
@@ -15,9 +15,29 @@ export default (appInfo: EggAppInfo) => {
     sourceUrl: `https://github.com/eggjs/examples/tree/master/${appInfo.name}`,
   };
 
+  config.cors = {
+    origin: () => "*",
+    allowMethods: "GET,POST,PUT,DELLETE,HEAD,PATCH",
+    credentials: true,
+  };
+
+  config.security = {
+    csrf: {
+      enable: false,
+    },
+  };
+
+  const userConfig = {
+    APP_KEY: "6318dadab57758112f37089063842f9a",
+    API: {
+      GET_NEWS_LIST: "http://v.juhe.cn/toutiao/index",
+    },
+  };
+
   // the return config will combines to EggAppConfig
   return {
     ...config,
     ...bizConfig,
+    ...userConfig,
   };
 };
